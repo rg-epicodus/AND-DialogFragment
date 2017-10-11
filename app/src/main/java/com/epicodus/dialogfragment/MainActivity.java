5526297d35f59e3f8829e073178312b0c4aa818e
Rich Garrick <richg341@gmail.com>
2017-10-11 11:08:46 -0700

    add radio button diolog fragment

--- app/src/main/java/com/epicodus/dialogfragment/MainActivity.java
+++ app/src/main/java/com/epicodus/dialogfragment/MainActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.dialogfragment;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.moodButton)
+    Button moodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        moodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/epicodus/dialogfragment/MoodDialogFragment.java
@@ -0,0 +1,49 @@
+package com.epicodus.dialogfragment;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+/**
+ * Created by OIG on 10/11/2017.
+ */
+
+public class MoodDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        final RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int selectedId = surveyRadioGroup.getCheckedRadioButtonId();
+                final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);
+                Log.d("testing output", selectedRadioButton.getText().toString());
+                dismiss();
+            }
+        });
+
+        return rootView;
+    }
+}
